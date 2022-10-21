92575083a2f945ba3b271fb114580b79faaf6615
Owen Adams <owenadams.main@gmail.com>
2022-10-21 02:21:34 +0000

    Define a parseRoute function. Import parseRoute to app jsx. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import Home from './pages/home';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
   render() {
     return <Home />;
   }
