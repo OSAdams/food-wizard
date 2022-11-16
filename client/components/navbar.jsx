0e78f3bcc74782bfa440edb271785116a8af432e
Owen Adams <owenadams.main@gmail.com>
2022-11-16 02:49:54 +0000

    create navbar component. define onchange method. WIP. #10

---
+++ client/components/navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState({ keyword: value });
+  }
+
+  render() {
+    const { handleChange } = this;
+    return (
+      <div className="nav-bar">
+        <div className="nav-menu-icon">
+          <i className="far fa-bars" />
+        </div>
+        <div className="nav-search">
+          <form className="nav-search-form">
+            <div>
+              <label htmlFor="keyword" />
+              <input
+                required
+                id="keyword"
+                type="text"
+                name="keyword"
+                onChange={ handleChange }
+                className="nav-input" />
+            </div>
+            <div>
+              <button type="submit">
+                <p>Search</p>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
