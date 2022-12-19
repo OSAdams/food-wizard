f565b373206f52df568b99a49af4cd107af34565
Owen Adams <owenadams.main@gmail.com>
2022-12-19 00:17:46 +0000

    update fetch req url, disable eslint. #10

--- client/pages/search.jsx
+++ client/pages/search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RecipeCard from '../components/recipe-card';
 
 export default class Search extends React.Component {
   constructor(props) {
@@ -11,22 +12,30 @@ export default class Search extends React.Component {
   componentDidMount() {
     // eslint-disable-next-line
     const keyword = this.props.keyword;
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${keyword}&apiKey=${process.env.SPOONACULAR_API_KEY}&number=10`)
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${keyword}&apiKey=${process.env.SPOONACULAR_API_KEY}&number=10&addRecipeNutrition=true`)
       .then(res => res.json())
       .then(recipes => this.setState({ recipes }))
       .catch(err => console.error({ error: err }));
   }
 
   render() {
-    // eslint-disable-next-line no-console
-    console.log('search props:', this.props.keyword);
-    // eslint-disable-next-line no-console
-    console.log('get req result:', this.state.recipes.results);
     if (this.state.recipes.length < 1) {
       return <h1>WhAt&apos;S gOiNg On HeRe</h1>;
     }
-    const recipeTitles = this.state.recipes.results.map(index => {
-      return <h3 key={ index.id }>{ index.title }</h3>;
+    const { results } = this.state.recipes;
+    // eslint-disable-next-line
+    console.log(results);
+    const recipeTitles = results.map(index => {
+      return (
+        <RecipeCard
+          key={ index.id }
+          title={ index.title }
+          servings={ index.servings }
+          likes={ index.aggregateLikes }
+          image={ index.image }
+          time={ index.readyInMinutes }
+          diet={ index.diets } />
+      );
     });
     return (
       <div>
