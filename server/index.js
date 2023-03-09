e40c419d4548cfd96527f4947298aa5527818bde
Owen Adams <owenadams.main@gmail.com>
2023-03-09 02:03:04 +0000

    update index server code to allow a user to sign in, receiving a signed token. WIP. #13

--- server/index.js
+++ server/index.js
@@ -79,37 +79,34 @@ app.post('/api/auth/sign-up', (req, res, next) => {
     .catch(err => next(err));
 });
 
-app.post('/api/aught/sign-in', (req, res, next) => {
+app.post('/api/auth/sign-in', (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    throw new ClientError(400, 'username and password are required fields');
+    throw new ClientError(401, 'username and password are required fields');
   }
-  argon2
-    .hash(password)
-    .then(hashedPassword => {
-      const sql = `
-             SELECT "userId",
-                    "hasedhPassword"
-             FROM users
-            WHERE username = #1
-      `;
-      const params = [username];
-      db.query(sql, params)
-        .then(result => {
-          const [user] = result.rows;
-          if (!user) {
+  const sql = `
+    SELECT "userId",
+           "hashedPassword"
+      FROM "users"
+     WHERE "username" = $1
+  `;
+  const params = [username];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'username or password are invalid');
+      }
+      const { userId, hashedPassword } = user;
+      return argon2
+        .verify(hashedPassword, password)
+        .then(isMatching => {
+          if (!isMatching) {
             throw new ClientError(401, 'username or password are invalid');
           }
-          const { userId, hashedPassword } = user;
-          return argon2.verify(hashedPassword, password)
-            .then(isMatching => {
-              if (!isMatching) {
-                throw new ClientError(401, 'username or password are invalid');
-              }
-              const payload = { userId, username };
-              const token = jwt.sign(payload, process.env.TOKEN_SECRET);
-              res.json({ token, user: payload });
-            });
+          const payload = { userId, username };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
         });
     })
     .catch(err => next(err));
