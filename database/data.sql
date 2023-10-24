b15b522f1ee35c00d0e9ec54a57fa057ed89a45d
Owen Adams <owenadams.main@gmail.com>
2023-10-24 21:18:52 +0000

    add comment in data.sql to prepare for account testing. WIP

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+/*
+Create a 3 accounts to test commenting, and commenting features.
+Once accounts are created, post on 3 recipes with all the accounts.
+
+Log the recipes that were commented on below this line,
+using recipeId and title.
+*/
