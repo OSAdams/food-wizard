28712fb77c43cd6164e009bef10ef03a2dd42ee2
Owen Adams <owenadams.main@gmail.com>
2023-11-09 13:19:08 +0000

    create delete component, create skeleton. WIP. #32

---
+++ client/components/delete-comment-modal.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function DeleteCommentModal(props) {
+  return (
+    <div className="delete-modal-background">
+      <div className="delete-modal">
+        <div className="delete-modal-warning">
+          <p>
+            Warning: Are you sure you want to delete your comment? This action is irreversible.
+          </p>
+        </div>
+        <div className="delete-modal-actions">
+          <button className="delete-modal-cancel">
+            Oops, take me back!
+          </button>
+          <button className="delete-modal-confirm">
+            Delete!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
